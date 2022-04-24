88dd260827947cb5d7e7a5bb0d986825f1722094
Vanmathidhina <73590110+Vanmathidhina@users.noreply.github.com>
2022-04-24 15:58:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+// class="tooltip" title="Patmos"
+
+// $(document).ready(function() {
+//     $('.tooltip').tooltipster({});
+// });
+ 
+//  $(document).ready(function(){
+//     $('[data-toggle="popover"]').popover({
+//         trigger : 'hover',
+//         placement : 'top',
+//     });   
+// });
+
+function load_angmokio(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="angmokio.html" > </object>';
+
+}
+
+function load_choachukang(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="choachukang.html" > </object>';
+
+}
+
+
+function  load_littleindia(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="littleindia.html" > </object>';
+
+}
+
+function  load_neesoon(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="neesoon.html" > </object>';
+
+}
+
+function  load_neesoon(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="neesoon.html" > </object>';
+
+}
+function  load_sembawag(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="sembawag.html" > </object>';
+
+}
+
+function  load_serangoon(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="seranoon.html" > </object>';
+
+}
+
+function  load_tuas(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="tuas.html" > </object>';
+
+}
+
+function  load_ubin(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="ubin.html" > </object>';
+
+}
+
+function  load_woodlands(){
+    document.getElementById("description").innerHTML='<object type="text/html" 
+    data="woodlands.html" > </object>';
+
+}
+
+
+
+
+
+
